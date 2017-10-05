3b8446301a235f9718c1161947efa3cac9292407
Babyboom <thebabyboom@gmail.com>
2017-10-05 22:06:29 +0200

    fin exercice 5

--- j02/ft_print_comb2.c
+++ j02/ft_print_comb2.c
@@ -17,40 +17,33 @@ int ft_putchar(char c)
 void	ft_print_comb2(void)
 {
 	int	numbers[4] = {48, 48, 48, 48};
-	ft_putchar(numbers[0]);
-	ft_putchar(numbers[1]);
-	ft_putchar(numbers[2]);
-	ft_putchar(numbers[3]);
-	
-	while (numbers[0] <= 57)
+		
+	while ((numbers[0] <= 57) && (numbers[1] <= 57))
 	{
-		if(numbers[0] < numbers[3])
-		{
 			ft_putchar(numbers[0]);
 			ft_putchar(numbers[1]);
+			ft_putchar(' ');
 			ft_putchar(numbers[2]);
 			ft_putchar(numbers[3]);
-			
-			if (numbers[0] != 57 && numbers[3] != 57)
-				ft_putchar(',');
-			if (numbers[0] != 57)
+			if ((numbers[0] != 57) || (numbers[1] != 57) || (numbers[2] != 57) || (numbers[3] != 57) )
+			{
 				ft_putchar(' ');
-		}
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
 		
 		if (numbers[3]++ >= 57)
 		{
 			numbers[3] = 48;
-			numbers[2]++;
-		}
-		if (numbers[2]++ >= 57)
-		{
-			numbers[2] = 48;
-			numbers[1]++;
-		}
-		if (numbers[1] == 58)
-		{
-			numbers[1] = 48;
-			numbers[0]++;
+			if (numbers[2]++ >= 57)
+			{
+				numbers[2] = 48;
+				if (numbers[1]++ >= 57)
+				{
+					numbers[1] = 48;
+					numbers[0]++;
+				}
+			}
 		}
 	}
 }
