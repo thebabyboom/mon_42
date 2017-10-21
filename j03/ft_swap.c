e1072d171f68b69e814154c04a1d482b24bc47db
Babyboom <thebabyboom@gmail.com>
2017-10-21 15:30:33 +0200

    ajout de l'ex06 avec la fonction

--- j03/ft_swap.c
+++ j03/ft_swap.c
@@ -4,5 +4,15 @@
  * @Email:  thebabyboom@gmail.com
  * @Filename: ft_swap.c
  * @Last modified by:   Babyboom
- * @Last modified time: 21/10/2017  03:23:01
+ * @Last modified time: 21/10/2017  03:30:11
  */
+void ft_swap(int *a, int *b);
+
+void ft_swap(int *a, int *b)
+{
+  int temp=0;
+
+  temp=*a;
+  *a = *b;
+  *b=temp;
+}
