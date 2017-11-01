c4f16b43a4787b76d03203526544c2e40f7fc939
Babyboom <thebabyboom@gmail.com>
2017-11-01 12:46:52 +0100

    ajout ex03

--- j04/ft_recursive_power.c
+++ j04/ft_recursive_power.c
@@ -4,7 +4,7 @@
  * @Email:  thebabyboom@gmail.com
  * @Filename: ft_recursive_power.c
  * @Last modified by:   Coléry David
- * @Last modified time: 01/11/2017  12:23:27
+ * @Last modified time: 01/11/2017  12:46:23
  */
  #include <unistd.h>
  #include <stdio.h>
@@ -14,21 +14,25 @@
  void main()
  {
    int res=0;
-   res=ft_recursive_power(4,1);
+   res=ft_recursive_power(5,4);
    printf("%d\n",res);
  }
 
  int ft_recursive_power(int nb, int power)
  {
    int res=1;
-   if (power>0)
+   if (power>=0)
    {
-     res=nb*ft_recursive_power(nb,power-1);
+     if(power>0)
+     {
+       res=nb;
+       res=res*ft_recursive_power(nb,power-1);
+     }
      return(res);
    }
    else
    {
-     res=1;
+     res=0;
      return(res);
    }
  }
