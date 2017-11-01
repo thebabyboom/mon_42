2d15b6449ebe9fdd97fb92753f224def2d62c73b
Babyboom <thebabyboom@gmail.com>
2017-11-01 11:21:23 +0100

    Jour04 la récursivité ex00

---
+++ j04/ft_iterative_factorial.c
@@ -0,0 +1,32 @@
+/**
+ * @Author: Coléry David
+ * @Date:   01/11/2017  10:52:58
+ * @Email:  thebabyboom@gmail.com
+ * @Filename: ft_iterative_factorial.c
+ * @Last modified by:   Coléry David
+ * @Last modified time: 01/11/2017  11:19:04
+ */
+
+#include <unistd.h>
+#include <stdio.h>
+
+int ft_iterative_factorial(int nb);
+
+void main()
+{
+  int res=0;
+  res=ft_iterative_factorial(5);
+  printf("%d\n",res);
+
+}
+int ft_iterative_factorial(int nb)
+{
+  int i=0;
+  int res=0;
+  res=nb;
+  for (i=1;i<nb-1;i++)
+  {
+    res=res*(nb-i);
+  }
+  return res;
+}
