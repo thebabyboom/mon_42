3dd00fc887b9f9666455074e62075bfe87b1abbd
Babyboom <thebabyboom@gmail.com>
2017-11-01 11:28:06 +0100

    gestion de l'erreur dans l'ex00

---
+++ j04/ft_recursive_factorial.c
@@ -0,0 +1,8 @@
+/**
+ * @Author: Coléry David
+ * @Date:   01/11/2017  11:21:36
+ * @Email:  thebabyboom@gmail.com
+ * @Filename: ft_recursive_factorial.c
+ * @Last modified by:   Coléry David
+ * @Last modified time: 01/11/2017  11:21:36
+ */
