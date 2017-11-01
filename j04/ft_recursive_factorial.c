af5e13bff7555948c8b7788c56ef6f16f366c5de
Babyboom <thebabyboom@gmail.com>
2017-11-01 11:30:08 +0100

    sauvegarde

--- j04/ft_recursive_factorial.c
+++ j04/ft_recursive_factorial.c
@@ -4,5 +4,32 @@
  * @Email:  thebabyboom@gmail.com
  * @Filename: ft_recursive_factorial.c
  * @Last modified by:   Coléry David
- * @Last modified time: 01/11/2017  11:21:36
+ * @Last modified time: 01/11/2017  11:29:42
  */
+
+#include <unistd.h>
+#include <stdio.h>
+
+int ft_recursive_factorial(int nb);
+
+void main()
+{
+  int res=0;
+  res=ft_recursive_factorial(5);
+  printf("%d\n", res);
+}
+
+int ft_recursive_factorial(int nb)
+{
+  int res=0;
+  if(nb>1)
+  {
+    res=nb*ft_recursive_factorial(nb-1);
+    return(res);
+  }
+  else
+  {
+    res=nb;
+    return(res);
+  }
+}
