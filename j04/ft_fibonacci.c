118757cc6087cf4fd3b533a18037ede80db5ee4c
Babyboom <thebabyboom@gmail.com>
2017-11-01 13:19:36 +0100

    ajout ex04

---
+++ j04/ft_fibonacci.c
@@ -0,0 +1,38 @@
+/**
+ * @Author: Coléry David
+ * @Date:   01/11/2017  01:04:45
+ * @Email:  thebabyboom@gmail.com
+ * @Filename: ft_fibonacci.c
+ * @Last modified by:   Coléry David
+ * @Last modified time: 01/11/2017  01:17:58
+ */
+ #include <unistd.h>
+ #include <stdio.h>
+
+ int ft_fibonacci(int index);
+
+ void main()
+ {
+   int res=0;
+   res=ft_fibonacci(10);
+   printf("%d\n",res);
+ }
+
+int ft_fibonacci(int index)
+{
+  int res;
+  if (index==0)
+    res=0;
+  else
+  {
+    if(index==1 || index==2)
+    {
+      res=1;
+    }
+    else
+    {
+      res=ft_fibonacci(index-1)+ft_fibonacci(index-2);
+    }
+  }
+  return(res);
+}
